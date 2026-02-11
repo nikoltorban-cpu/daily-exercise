0f33fbaa40604c673dd632931da5fd8538026853
Nikol Torban <nikoltorban@be:7e:7a:ed:82:d4.home>
2026-02-11 10:10:32 +0200

    my first commit

---
+++ new.py
@@ -0,0 +1,7 @@
+number = int(input("pick a number between 1 and 100 "))
+if number % 3 == 0 and number % 5 == 0:
+    print("fizzbuzz")
+elif number % 3 == 0:
+    print("fizz")
+elif number % 5 == 0:
+    print("buzz")
