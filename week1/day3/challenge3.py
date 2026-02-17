ed0a5e3630de0e05380282e106943dd21682f864
Nikol Torban <nikoltorban@96:24:83:42:72:ae.home>
2026-02-17 16:16:33 +0200

    daily challenge

--- week1/day3/challenge3.py
+++ week1/day3/challenge3.py
@@ -6,7 +6,22 @@ for i in range(len(letter)):
         new_dict[letter[i]] = []
     new_dict[letter[i]].append(i)
 
-
 print(new_dict)
 
+#affordable items
+items_purchase = {'bread' : '1$', 'eggs' : '3$', 'milk' : '1$', 'cheese' : '2$', 'bannana' : '2$', 'tv' : '3,000$', 'pan' : '1,800$', 'sofa' : '7,560$', 'toilet' : '4280$', 'dishwasher' : '380$', 'sponge' : '3$', 'litter box' : '36$', 'lamp' : '560$'}
+wallet = int(input("how much money do you have? "))
 
+for item, price in items_purchase.items():
+    items_purchase[item] = int(price.replace('$', '').replace(',', ''))
+
+basket = []
+for item, price in items_purchase.items():
+    if items_purchase[item] <= wallet:
+         basket.append(item)
+         wallet -= items_purchase[item]
+
+if not basket:
+    print("no items can be afforded")
+else:         
+    print("you can afford: " + str(sorted(basket)))
