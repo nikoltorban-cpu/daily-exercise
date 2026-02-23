238ee88cab17703d68558584bd43d6145b9bc15f
Nikol Torban <nikoltorban@be:7e:7a:ed:82:d4.home>
2026-02-23 16:23:12 +0200

    hangman

---
+++ week1/day5/hangMan.py
@@ -0,0 +1,107 @@
+#hangman game
+def print_word(word):
+    guess = ["_ "] * len(word)
+    print("this is your board:")
+    print("*******************")
+    print(guess)
+
+
+
+hangman_stages = [
+"""
+  -----
+  |   |
+      |
+      |
+      |
+      |
+---------
+""",
+"""
+  -----
+  |   |
+  O   |
+      |
+      |
+      |
+---------
+""",
+"""
+  -----
+  |   |
+  O   |
+  |   |
+      |
+      |
+---------
+""",
+"""
+  -----
+  |   |
+  O   |
+ /|   |
+      |
+      |
+---------
+""",
+"""
+  -----
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+---------
+""",
+"""
+  -----
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+---------
+""",
+"""
+  -----
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+---------
+"""
+]
+
+
+import random
+
+wordslist = ['correction', 'childish', 'beach', 'python', 'assertive', 'interference', 'complete', 'share', 'credit card', 'rush', 'south']
+word = random.choice(wordslist)
+mistakes = 0
+print_word(word)
+new_guess = [" "] * len(word)
+while mistakes < 6:
+    if mistakes == 0:
+        print(hangman_stages[0])
+    guess = input("guess a letter: ")
+    for i in range(len(word)):
+        if guess == word[i]:
+            new_guess[i] = guess
+    if new_guess:
+        print(new_guess)
+        if "".join(new_guess) == word:
+            print("you win")
+            break
+   
+            
+
+        
+    if guess not in word:
+        print("wrong")
+        mistakes += 1
+        print(hangman_stages[mistakes])
+
+if mistakes == 6:
+    print(hangman_stages[6])
+    print("you lose, the word was " + word)
